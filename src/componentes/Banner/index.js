4f5e984252e0a7cf81f9ba82030ac7d261aae86d
Nickgalant <nicholasgalante@hotmail.co.uk>
2022-07-17 19:45:30 -0300

    uploading Organo website

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX document.createElement('img's)
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner;
